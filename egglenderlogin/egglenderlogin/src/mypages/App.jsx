c92106c8e91440fc0fab30a22254a788ce870492
chenda duan <chenda@ucla.edu>
2020-11-16 12:27:14 +0800

    chenda_first_commit
    Finished the basic framework

---
+++ egglenderlogin/egglenderlogin/src/mypages/App.jsx
@@ -0,0 +1,9 @@
+import React, {Component} from 'react'
+import eggNavigator from './'
+export default class App extends Component{
+    render(){
+        return(
+            
+        )
+    }
+}
---
+++ egglenderlogin/egglenderlogin/src/mypages/login/index.jsx
@@ -0,0 +1,10 @@
+import React, {Component} from 'react'
+export default class eggLogin extends Component{
+    render(){
+        return(
+            <div>
+                placeHolder for EggLogin
+            </div>
+        )
+    }
+}
