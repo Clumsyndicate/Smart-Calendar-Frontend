020c09d8117075741ad71f3ea5b82ffc85620ce1
chenda duan <chenda@ucla.edu>
2020-11-23 17:03:31 +0800

    Update Register and Storing some data
    Update the signup page so that it has more content than signin. Also add somet welcome words for homepage

--- egglenderlogin/egglenderlogin/src/mypages/login/index.jsx
+++ egglenderlogin/egglenderlogin/src/mypages/login/index.jsx
@@ -4,20 +4,21 @@ export default class eggLogin extends Component{
         return(
             <div>
                 <form>
-                    <div class="form-group">
-                        <label for="exampleInputEmail1">Username or Email address</label>
-                        <input type="email" class="form-control" id="useremail" aria-describedby="emailHelp"/>
-                        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                    <div className="form-group">
+                        <label htmlFor="exampleInputEmail1">Username or Email address</label>
+                        <input type="email" className="form-control" id="useremail" aria-describedby="emailHelp"/>
+                        <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
                     </div>
-                    <div class="form-group">
-                        <label for="userpwd">Password</label>
-                        <input type="password" class="form-control" id="userpwd"/>
+                    <div className="form-group">
+                        <label htmlFor="userpwd">Password</label>
+                        <input type="password" className="form-control" id="userpwd"/>
                     </div>
-                    <div class="form-group form-check">
-                        <input type="checkbox" class="form-check-input" id="usercheck"/>
-                        <label class="form-check-label" for="usercheck">Remember me</label>
+                    <div className="form-group form-check">
+                        <input type="checkbox" className="form-check-input" id="usercheck"/>
+                        <label className="form-check-label" htmlFor="usercheck">Remember me</label>
                     </div>
-                    <button type="submit" class="btn btn-primary">Login</button>
+                    <button type="submit" className="btn btn-primary">Login</button>
+                    <a className="btn" href="/signup" role="button">Sign Up </a>
                 </form>
             </div>
         )
--- egglenderlogin/egglenderlogin/src/mypages/signup/index.jsx
+++ egglenderlogin/egglenderlogin/src/mypages/signup/index.jsx
@@ -1,9 +1,39 @@
 import React, {Component} from 'react'
 export default class eggSignup extends Component{
+    state = {
+        userName: '',
+        userEmail: '',
+        userPwd: '',
+        userPwd2: '',
+    };
     render(){
+        const { userName, userEmail, userPwd, userPwd2} = this.state;
         return(
             <div>
-                placeHolder for EggRegister
+                <form>
+                    <div className="form-group">
+                        <label htmlFor="userName">Username</label>
+                        <input type="email" className="form-control" id="userName" defaultValue={userName}/>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="userEmail">Email address</label>
+                        <input type="email" className="form-control" id="userEmail" defaultValue={userEmail}/>
+                        <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="userPwd">Password</label>
+                        <input type="password" className="form-control" id="userPwd" defaultValue={userPwd}/>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="userPwd2">Confirm Password</label>
+                        <input type="password" className="form-control" id="userPwd2" defaultValue={userPwd2}/>
+                    </div>
+                    <div className="form-group form-check">
+                        <input type="checkbox" className="form-check-input" id="usercheck"/>
+                        <label className="form-check-label" htmlFor="usercheck">Remember me</label>
+                    </div>
+                    <button type="submit" className="btn btn-primary">Login</button>
+                </form>
             </div>
         )
     }
