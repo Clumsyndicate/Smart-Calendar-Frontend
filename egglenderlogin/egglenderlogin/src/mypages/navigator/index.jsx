a37e46cf14fa1e9aac12da902febaa884954ee19
chenda duan <chenda@ucla.edu>
2020-11-25 23:25:07 +0800

    add some handle function

--- egglenderlogin/egglenderlogin/src/mypages/navigator/index.jsx
+++ egglenderlogin/egglenderlogin/src/mypages/navigator/index.jsx
@@ -4,7 +4,7 @@ export default class EggNavigator extends Component{
     render(){
         return(
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <a className="navbar-brand" href="#">Egglender</a>
+                <a className="navbar-brand" href="###">Egglender</a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
@@ -12,10 +12,10 @@ export default class EggNavigator extends Component{
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav mr-auto">
                     <li className="nav-item active">
-                        <Link className="nav-link" to="/Homepage">Home <span class="sr-only">(current)</span></Link>
+                        <Link className="nav-link" to="/Homepage">Home <span className="sr-only">(current)</span></Link>
                     </li>
                     <li className="nav-item dropdown">
-                        <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <a className="nav-link dropdown-toggle" href="####" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         User
                         </a>
                         <div className="dropdown-menu" aria-labelledby="navbarDropdown">
