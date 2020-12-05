3833b5c968f3684d61aeaf3447554b9acc12b05e
Wenjie Mo <54966329+rmartin233@users.noreply.github.com>
2020-12-05 17:25:38 +0800

    Bugs fixed
    Following bugs are fixed:
    - Time zone and display failure
    - disappear of programs

--- egglenderlogin/egglenderlogin/src/calendar-layout/App.js
+++ egglenderlogin/egglenderlogin/src/calendar-layout/App.js
@@ -2,17 +2,17 @@ import React from 'react';
 import moment from "moment";
 import Scheduler, { Editing } from 'devextreme-react/scheduler';
 import SelectBox from 'devextreme-react/select-box';
-import CheckBox from 'devextreme-react/check-box';
-import List, { ItemDragging } from 'devextreme-react/list';
+import axios from 'axios';
 import { events } from './events.js';
-
 import { data, locations } from './data.js';
 import timeZoneUtils from 'devextreme/time_zone_utils';
+//list
+import List, { ItemDragging } from 'devextreme-react/list';
 
 const currentDate = new Date(moment().format("YYYY"), parseInt(moment().format("MM")) -1,  moment().format('DD'));
-var txt = new String(moment().format("ddd MMM DD"));
+var txt = moment().format("ddd[,] MMM DD").toString();
 
-const views = ['week', 'month', 'day'];
+const views = ['week', 'month', 'timelineWeek'];
 
 
 function getLocations(date) {
@@ -22,7 +22,16 @@ function getLocations(date) {
   });
 }
 
-const demoLocations = getLocations(currentDate);
+//const demoLocations = getLocations(currentDate);
+var demoLocationstemp = getLocations(currentDate);
+demoLocationstemp[0].offset = 6;
+demoLocationstemp[0].id = "America/Glace_Bay";
+demoLocationstemp[3].id = "Etc/GMT-12";
+const demoLocations = demoLocationstemp;
+
+function ItemTemplate(data) {
+  return <div>{data.text}</div>;
+}
 
 class App extends React.Component {
   constructor(props) {
@@ -32,13 +41,78 @@ class App extends React.Component {
       demoLocations: demoLocations,
       //for list
       deleteType: 'slideItem',
-      selectedItems: [],
+      searchMode: 'contains',
+      events,
     };
     this.onValueChanged = this.onValueChanged.bind(this);
     this.onAppointmentFormOpening = this.onAppointmentFormOpening.bind(this);
     this.onOptionChanged = this.onOptionChanged.bind(this);
     //for list
     this.onSelectedItemsChange = this.onSelectedItemsChange.bind(this);
+    this.onDragStart = this.onDragStart.bind(this);
+    this.onAdd = this.onAdd.bind(this);
+    this.onRemove = this.onRemove.bind(this);
+    this.onReorder = this.onReorder.bind(this);
+  }
+
+  state = {
+    loading: true,
+    error: "",
+    datab: null,
+    eventsb: null,
+  };
+
+  loadData = () => {
+    this.setState({ loading: true });
+    return axios
+      .get(
+        `https://5fc7ab11f3c77600165d8a61.mockapi.io/text`
+      )
+      .then(result => {
+        //console.log(result);
+        this.setState({
+          datab: result.data,
+          loading: false,
+          error: false
+        });
+      })
+      .catch(error => {
+        console.error("error: ", error);
+        this.setState({
+          // objects cannot be used as a react child
+          // -> <p>{error}</p> would throw otherwise
+          error: `${error}`,
+          loading: false
+        });
+      });
+  };
+
+  loadList = () => {
+    this.setState({ loading: true });
+    return axios
+      .get(
+        `https://5fc9fe933c1c22001644175c.mockapi.io/events`
+      )
+      .then(result => {
+        console.log(result.data.text);
+        this.setState({
+          eventsb: result.data,
+          loading: false,
+          error: false
+        });
+      })
+      .catch(error => {
+        console.error("error: ", error);
+        this.setState({
+          error: `${error}`,
+          loading: false
+        });
+      });
+  };
+
+  componentDidMount() {
+    this.loadData();
+    this.loadList();
   }
 
   onValueChanged(e) {
@@ -46,7 +120,6 @@ class App extends React.Component {
       timeZone: e.value
     });
   }
-
   onAppointmentFormOpening(e) {
     const form = e.form;
 
@@ -61,7 +134,6 @@ class App extends React.Component {
     startDateDataSource.load();
     endDateDataSource.load();
   }
-
   onOptionChanged(e) {
     if(e.name === 'currentDate') {
       this.setState({
@@ -71,6 +143,7 @@ class App extends React.Component {
   }
 
 // for list
+
   onSelectedItemsChange(args) {
     if(args.name === 'selectedItems') {
       this.setState({
@@ -78,19 +151,38 @@ class App extends React.Component {
       });
     }
   }
-
+  onDragStart(e) {
+    e.itemData = 
+    this.state[e.fromData][e.fromIndex];
+  }
+  onAdd(e) {
+    const tasks = this.state[e.toData];
+    this.setState({
+      [e.toData]: [...tasks.slice(0, e.toIndex), e.itemData, ...tasks.slice(e.toIndex)]
+    });
+  }
+  onRemove(e) {
+    const tasks = this.state[e.fromData];
+    this.setState({
+      [e.fromData]: [...tasks.slice(0, e.fromIndex), ...tasks.slice(e.fromIndex + 1)]
+    });
+  }
+  onReorder(e) {
+    this.onRemove(e);
+    this.onAdd(e);
+  }
 
   render() {
-    const { timeZone, demoLocations } = this.state;
+    const { timeZone, demoLocations, eventsb, loading, error, datab } = this.state;
     return (
       <React.Fragment>
-
+        <div className="Title">MY EGGLENDAR</div>
         <div class="aParent">
-
           <div className="calendar-layout">
             <div className="option">
-              <span className="caption">{txt} 「 Time Zone 」:</span>
+              <span className="caption">{txt} 「 Time Zone 」</span>
               <SelectBox
+                className="selector"
                 items={demoLocations}
                 displayExpr="title"
                 valueExpr="id"
@@ -99,18 +191,17 @@ class App extends React.Component {
                 onValueChanged={this.onValueChanged}
               />
             </div>
-
             <Scheduler
-              dataSource={data}
+              dataSource={datab}
               views={views}
               defaultCurrentView="week"
-              startDayHour={0}
+              startDayHour={7}
               defaultCurrentDate={currentDate}
               timeZone={timeZone}
-              height={800}
+              height={630}
+              showAllDayPanel={true}
               onAppointmentFormOpening={this.onAppointmentFormOpening}
-              onOptionChanged={this.onOptionChanged}
-            >
+              onOptionChanged={this.onOptionChanged}>
               <Editing
                 allowTimeZoneEditing={true}
               />
@@ -118,27 +209,35 @@ class App extends React.Component {
           </div>
 
           <div className="widget-container">
-            <div className="header">
-              <span className="caption">Upcoming Dues !!! </span>
-              <div>( You could slide to delete or drag to change priority !) </div>
-            </div>
 
+            <div className="header">
+              <span className="caption">Upcoming Events !!! </span>
+              <div className="sideNote">( Slide left to delete, or drag to change 
+               priority, or just put a check in the box !)</div>
+            </div>
             <List
-              items={events}
-              height={600}
+              dataSource={eventsb}
+              height={630}
+              keyExpr="id"
+              repaintChangesOnly={true}
               allowItemDeleting={true}
               itemDeleteMode={this.state.deleteType}
               showSelectionControls={true}
               selectionMode="multiple"
-              selectedItems={this.state.selectedItems}
-              onOptionChanged={this.onSelectedItemsChange}>
+              onOptionChanged={this.onSelectedItemsChange}
+              itemRender={ItemTemplate}
+              searchExpr="text"
+              searchEnabled={true}
+              searchMode={this.state.searchMode}>
+              <ItemDragging
+                allowReordering={true}
+                data="eventsb"
+                onDragStart={this.onDragStart}
+                onAdd={this.onAdd}
+                onRemove={this.onRemove}
+                onReorder={this.onReorder}>
+              </ItemDragging>
             </List>
-
-            <div className="selected-data">
-              <span className="caption">Completed Tasks: </span>
-              <span>{this.state.selectedItems.join(', ')}</span>
-            </div>
-
           </div>
 
         </div>
@@ -147,7 +246,5 @@ class App extends React.Component {
     );
   }
 }
-
-
 
 export default App;
